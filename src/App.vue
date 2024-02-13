b2965f53d78010da3a0fe74b3c242e8225710bd3
Thrisler <alexandros.gn@outlook.de>
2024-02-13 10:39:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+   import rickmortyimg from './assets/rick-and-morty-portal-moon-mod-download-35.png';
+   const title = 'Rick and Morty';
+</script>
+
+<template>
+
+  <div class="container mt-3 p-4 bg-danger text-white reounded text-center">
+    <h1>{{title}}</h1>
+  </div>
+
+  <div class="container mt-3 p-4 bg-dark text-white reounded text-center">
+    <img :src="rickmortyimg" alt="Rick and Morty" class="img-fluid">
+    <p>Numeber of characters: {{characters.info.count}}</p>
+    <p>{{temp}}</p>
+  </div>
+
+  <div class="container mt-3 rounded text-center">
+    <table class="table">
+      <tr>
+        <th>Name</th>
+        <th>Image</th>
+        <th>species</th>
+        <th>gender</th>
+        <th>origin-name</th>
+        <th>location-name</th>
+      </tr>
+      <tr v-for="character in characters.results" :key="character.id">
+        <td>{{character.name}}</td>
+        <td><img class="img-fluid" :src="character.image" alt="character.name"></td>
+        <td>{{character.species}}</td>
+        <td>{{character.gender}}</td>
+        <td>{{character.origin.name}}</td>
+        <td>{{character.location.name}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import {characters} from "@/assets/characters.js";
+const temp = [1,2,3,4,5,6,7,8,9,10];
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      characters,
+      temp
+    }
+  }
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+
+createApp(App).mount('#app')
